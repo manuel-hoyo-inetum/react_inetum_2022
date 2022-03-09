0eca9506e6f9c1738fb900085248e55ad91c60aa
Manuel Hoyo Estévez <manuel.hoyo@inetum.world>
2022-03-09 11:04:53 +0100

    Práctica2: añadir un botón

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+
+const Button = () => (
+  <TouchableHighlight>
+    <Text>Hello World</Text>
+  </TouchableHighlight>
+);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Texto modificado por Manolo... ahora con un botón</Text>
+      <Button></Button>
       <StatusBar style="auto" />
     </View>
   );
